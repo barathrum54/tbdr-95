ea2d49e7d85c8cc5676e7544bf8a65d8a6accad5
barathrum54 <tahabdurmus0@gmail.com>
2023-12-11 15:19:33 +0300

    feat: implemented shaders of CRT and Glass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Taskbar } from './components/taskbar/organisms/Taskbar';
 import { Desktop } from './components/desktop/organisms/Desktop';
 import { StartMenu } from './components/start-menu/organisms/StartMenu';
@@ -7,6 +7,10 @@ import { Shell } from './components/shell/organisms/Shell';
 function App() {
   return (
     <div className="App">
+      <div className="shaders">
+        <div className="crt"></div>
+        <div className="glass"></div>
+      </div>
       <Shell />
       <StartMenu />
       <Desktop />
